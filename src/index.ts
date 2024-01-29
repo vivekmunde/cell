25427cfca732731e8ab7da8145b69fd0bbb57d22
Vivek <vivekmunde@users.noreply.github.com>
2024-01-29 16:39:02 +0530

    Update intellisense comments

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,10 @@ import * as pusu from "pusu";
 
 /**
  * A subscriber function to be called each time the state change is published.
+ *
  * @param {TSelectedState} state - The selected state. If selector is not provided then the entire state is returned.
  */
-type TSubscriber<TSelectedState> = (
-  /** The selected state. If selector is not provided then the entire state is returned. */
-  state: TSelectedState
-) => void;
+type TSubscriber<TSelectedState> = (state: TSelectedState) => void;
 
 /**
  * An unsubscriber function, which when called unsubscribes the subscriber function from from the state updates.
@@ -16,36 +14,36 @@ type TUnsubscribe = () => void;
 
 /**
  * A selector function to select the required state value(s) from the state.
+ *
  * @param {TState} state - Latest state of the cell.
  * @returns {TSelectedState} Selected state.
  */
-type TSelector<TState, TSelectedState> = (
-  /** Latest state of the cell. */
-  state: TState
-) => TSelectedState;
+type TSelector<TState, TSelectedState> = (state: TState) => TSelectedState;
 
 /**
- * An equality comparater function to determine if the previous selected state and the current selected state are equal. If there is no change in the selected state then the subscribers will not be called.
+ * An equality comparator function which receives previous and current selected state.
+ * This equality comparater function can be used to determine if the current selected state has changed from the previous selected state.
+ * If there is no change in the selected state then the subscribers will not be called.
+ * True: Meaning the selected state has not changed.
+ * False: Meaning the selected state has changed.
+ * If not passed then it uses the default comparator function (current, previous) => (current === previous).
+ *
  * @param {TSelectedState} currentState - Current selected state of the cell.
  * @param {TSelectedState} previousState - Previous selected state of the cell.
- * @returns {boolean} A boolean result indicating if the selected state is changed or not. True = Equal meaning selected state is not changed. False = Not equal meaning selected state is changed.
+ * @returns {boolean} A boolean result indicating if the selected state has changed or not. True: Meaning selected state has not changed. False: Meaning selected state has changed.
  */
 type TAreEqual<TSelectedState> = (
-  /** Current selected state of the cell. */
   currentState: TSelectedState,
-  /** Previous selected state of the cell. */
   previousState: TSelectedState
 ) => boolean;
 
 /**
  * A reducer function which recieves the current state fo the cell and should return the updated state.
+ *
  * @param {function} state - Current state of the cell.
- * @returns {TState} Updated state.
+ * @returns {TState} Latest updated state.
  */
-type TReducer<TState> = (
-  /** Current state of the cell. */
-  state: TState
-) => TState;
+type TReducer<TState> = (state: TState) => TState;
 
 /**
  * Creates and returns a new cell object with publish & subscribe methods.
@@ -57,10 +55,18 @@ const cell = <TState>(
   initialState: TState
 ): {
   /**
+   * Updates the state in cell and calls all subscribers with the updated state.
+   *
+   * @param {TReducer} reducer - A reducer function which recieves the current state fo the cell and should return the updated state.
+   */
+  publish: (reducer: TReducer<TState>) => void;
+
+  /**
    * Subscribes to the updates in the state of the cell.
-   * @param {TSubscriber} subscriber - A subscriber function to be called each time the state change is published.
-   * @param {TSelector} selector - A selector function to select the required state value(s) from the state.
-   * @param {TAreEqual} areEqual - An equality comparater function to determine if the previous selected state and the current selected state has changed. If there is no change in the selected state then the subscribers will not be called.
+   *
+   * @param {TSubscriber} subscriber - A subscriber function to be called each time the state change is published. The subscriber function will recieve the data selected and returned by the selector function. If selector is not supplied then the subscruber function will receive the complete state.
+   * @param {TSelector} selector - A selector function to select the required state value(s) from the state and return the selected state.
+   * @param {TAreEqual} areEqual - An equality comparator function which receives previous and current selected state. This equality comparater function can be used to determine if the current selected state has changed from the previous selected state. If there is no change in the selected state then the subscribers will not be called. True: Meaning the selected state has not changed. False: Meaning the selected state has changed. If not passed then it uses the default comparator function (current, previous) => (current === previous).
    * @returns {TUnsubscribe} An unsubscriber function, which when called unsubscribes the subscriber function from from the state updates.
    */
   subscribe: <TSelectedState>(
@@ -68,12 +74,6 @@ const cell = <TState>(
     selector?: TSelector<TState, TSelectedState>,
     areEqual?: TAreEqual<TSelectedState>
   ) => TUnsubscribe;
-
-  /**
-   * Updates the state in cell and calls all subscribers with the updated state.
-   * @param {TReducer} reducer - A reducer function which recieves the current state fo the cell and should return the updated state.
-   */
-  publish: (reducer: TReducer<TState>) => void;
 } => {
   const publication = pusu.createPublication<TState>({ name: "cell" });
 
@@ -132,7 +132,7 @@ const cell = <TState>(
     pusu.publish(publication, currentState);
   };
 
-  return { subscribe, publish };
+  return { publish, subscribe };
 };
 
 export default cell;
